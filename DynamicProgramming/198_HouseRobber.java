ac65dd2ed1c796d9d8a9ba682d37801d6ec700c7
ShawnyXiao <772077730@qq.com>
2017-05-27 09:08:55 +0800

    Add 303_RangeSumQueryImmutable and 198_HouseRobber

---
+++ DynamicProgramming/198_HouseRobber.java
@@ -0,0 +1,20 @@
+public class Solution {
+    public int rob(int[] nums) {
+        if (0 == nums.length) {
+            return 0;
+        }
+        if (1 == nums.length) {
+            return nums[0];
+        }
+        if (2 == nums.length) {
+            return Math.max(nums[0], nums[1]);
+        }
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        dp[1] = Math.max(dp[0], nums[1]);
+        for (int i = 2; i < nums.length; i++) {
+            dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
+        }
+        return dp[nums.length - 1];
+    }
+}
