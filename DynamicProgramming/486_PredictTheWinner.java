c74f818f754a5c774a17d045818da18f02adab4c
ShawnyXiao <772077730@qq.com>
2017-05-29 09:26:54 +0800

    Add 486_PredictTheWinner

---
+++ DynamicProgramming/486_PredictTheWinner.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public boolean PredictTheWinner(int[] nums) {
+        int[][] dp = new int[nums.length][nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            dp[i][i] = nums[i];
+        }
+        for (int j = 1; j < nums.length; j++) {
+            for (int i = j - 1; i >= 0; i--) {
+                dp[i][j] = Math.max(nums[i] - dp[i + 1][j], nums[j] - dp[i][j - 1]);
+            }
+        }
+        return dp[0][nums.length -1] >= 0;
+    }
+}
